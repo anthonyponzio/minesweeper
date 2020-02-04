bbfc11741dc463be52e9aa0931d4d30ec96e27d0
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-04 17:42:25 -0500

    add Board::generate_board, only creates mines at the moment

--- board.rb
+++ board.rb
@@ -1,2 +1,12 @@
+require_relative "tile"
+
 class Board
+  def self.generate_board
+    # testing board filled with mines first
+    Array.new(3) { Array.new(3) { Tile.new(true) } }
+  end
+
+  def initialize
+    @board = Board.generate_board
+  end
 end
