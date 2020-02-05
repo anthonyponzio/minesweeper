27858fdcf28c7c12b12dfaeeff5b8784ff290a00
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-05 18:49:39 -0500

    remove unused methods in Game

--- game.rb
+++ game.rb
@@ -7,38 +7,6 @@ class Game
     @cursor = Cursor.new(20)
   end
 
-  # def valid_command?(input)
-  #   input.downcase == "f" || input.downcase == "r"
-  # end
-  
-  # def valid_pos?(input)
-  #   input.size == 2 &&
-  #   input.all? { |i| (0...@board.size).include?(i) }
-  # end
-
-  # def get_command
-  #   print "\nEnter a command (`f` to flag, `r` to reveal): "
-  #   gets.chomp
-  # end
-
-  # def get_pos
-  #   print "\nEnter a position (format `#,#` | e.g `2,0`): "
-  #   gets.chomp.split(",").map(&:to_i)
-  # end
-
-  # def get_input
-  #   command = get_command
-  #   pos = get_pos
-    
-  #   until valid_command?(command) && valid_pos?(pos)
-  #     puts "Invalid command or position, try again!"
-  #     command = get_command
-  #     pos = get_pos
-  #   end
-
-  #   [command, pos]  
-  # end
-
   def make_move(command, pos)
     case command
     when "flag"
